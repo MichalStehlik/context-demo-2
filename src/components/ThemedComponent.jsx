a35e8c1e925e33fa5c9e636c6612ccc995c6a814
Michal Stehlík <36696240+MichalStehlik@users.noreply.github.com>
2020-12-03 12:04:11 +0100

    Update ThemedComponent.jsx

--- src/components/ThemedComponent.jsx
+++ src/components/ThemedComponent.jsx
@@ -2,7 +2,7 @@ import {useContext} from "react";
 import {ThemeContext} from '../providers/ThemeProvider';
 
 const ThemedComponent = (props) => {
-    const {theme} = useContext(ThemeContext);
+    const {theme, setTheme} = useContext(ThemeContext);
     return (
         <div className={"component " + theme}>Komponenta</div>
     );
